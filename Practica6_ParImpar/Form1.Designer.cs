368f797cf5a80d3a5e212ab7637afc7d189b5a57
vanyamendoza16 <vanyamendoza16@gmail.com>
2025-10-30 23:29:13 -0500

    Add files via upload

---
+++ Practica6_ParImpar/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace Practica6_ParImpar
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            textBox1 = new TextBox();
+            button1 = new Button();
+            listBox1 = new ListBox();
+            label2 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(169, 71);
+            label1.Name = "label1";
+            label1.Size = new Size(131, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Escribe un número";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(175, 115);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(125, 27);
+            textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(344, 115);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 2;
+            button1.Text = "Verifica";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.Location = new Point(488, 71);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(125, 264);
+            listBox1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(270, 29);
+            label2.Name = "label2";
+            label2.Size = new Size(247, 20);
+            label2.TabIndex = 4;
+            label2.Text = "Verifica si un numero es par o impar";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label2);
+            Controls.Add(listBox1);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox textBox1;
+        private Button button1;
+        private ListBox listBox1;
+        private Label label2;
+    }
+}
---
+++ Practica6_ParImpar/Form1.cs
@@ -0,0 +1,28 @@
+namespace Practica6_ParImpar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int numero = int.Parse(textBox1.Text);
+
+            if (numero % 2 == 0)
+            {
+                listBox1.Items.Add(numero + " Par");
+            }
+            else
+            {
+                listBox1.Items.Add(numero + " Impar");
+            }
+
+            // Limpiar TextBox y poner foco para nuevo número
+            textBox1.Text = "";
+            textBox1.Focus();
+        }
+    }
+}
